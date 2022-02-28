70bdc1e2631b7c6783a4d54bcd5798134a9991df
Lucifer8729 <72292326+Lucifer8729@users.noreply.github.com>
2022-03-01 00:05:25 +0530

    finished SRMIST_CET_Circulurs page

--- srm-academia/pages/Circular_III_Year_BTECH/index.tsx
+++ srm-academia/pages/Circular_III_Year_BTECH/index.tsx
@@ -1,7 +1,75 @@
 import React from "react";
+import Image from "next/image";
+import img1 from "../../public/academia-hat.bmp";
+import img2 from "../../public/image2.bmp";
+
+import { Box, Typography } from "@mui/material";
 
 const index = () => {
-  return <div>Circular_III_Year_BTECH</div>;
+  return (
+    <Box
+      sx={{
+        backgroundColor: "azure",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        pt: 5,
+        pb: 5,
+      }}
+    >
+      <Box>
+        <Image alt="Lamp" src={img1} width="300" height="200" />
+      </Box>
+      <Box sx={{ width: "100%" }}>
+        <Typography sx={{ color: "red", mt: 2, mb: 2 }}>
+          Dear Students,
+        </Typography>
+        <Typography sx={{ color: "blue" }}>
+          Greetings from College of Engineering & Technology, SRMIST !!
+        </Typography>
+      </Box>
+      <Box sx={{ border: "solid 1px #1E90FF", width: "70%", mt: 5, mb: 2 }}>
+        <Typography sx={{ color: "blue", mt: 2, mb: 2, fontSize: "24px" }}>
+          👉
+          <a style={{ color: "red" }}>
+            Information for III year B.Tech(UG) Students:-
+          </a>
+          We are pleased to inform you that we will be opening the oncampus
+          classes from
+          <a style={{ color: "red" }}>14/03/2022 (Monday).</a> The classes will
+          be held through physical mode only. SOPs to be strictly adhered to.
+          Suitable travel plans can be made at your end. Those who have already
+          booked the hostels will be permitted to occupy their rooms from{" "}
+          <a style={{ color: "red" }}>1st of March, 2022.</a> No fresh bookings
+          for the hostel will be permitted. SRMIST awaits your return to
+          experience the lively campus.🙂
+        </Typography>
+      </Box>
+      <Typography sx={{ m: 4 }}>test test</Typography>
+      <Box sx={{ border: "solid 1px #1E90FF", width: "60%" }}>
+        <Typography sx={{ color: "red", fontSize: "24px" }}>
+          Hostel booking for III Year B.Tech, B.Arch and B. Des students,
+        </Typography>
+      </Box>
+      <Box sx={{ border: "solid 1px #1E90FF", width: "60%", mt: 0.25 }}>
+        <Typography sx={{ fontSize: "24px" }}>
+          Hostel booking portal:
+          <a style={{ color: "red", textDecoration: "underline" }} href="#">
+            Click Here
+          </a>
+        </Typography>
+      </Box>
+      <Box sx={{ border: "solid 1px #1E90FF", width: "60%", mt: 0.25, mb: 8 }}>
+        <Typography sx={{ fontSize: "24px" }}>
+          Use your Net ID and Password.
+        </Typography>
+      </Box>
+      <Box>
+        <Image alt="Lamp" src={img2} width="300" height="200" />
+      </Box>
+    </Box>
+  );
 };
 
 export default index;
--- srm-academia/pages/SRMIST_CET_Circulurs/index.tsx
+++ srm-academia/pages/SRMIST_CET_Circulurs/index.tsx
@@ -1,7 +1,79 @@
 import React from "react";
+import Image from "next/image";
+import img1 from "../../public/academia-hat.bmp";
+import img2 from "../../public/image2.bmp";
+
+import { Box, Typography } from "@mui/material";
 
 const circular = () => {
-  return <div>circular</div>;
+  return (
+    <Box
+      sx={{
+        backgroundColor: "azure",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        pt: 5,
+        pb: 5,
+      }}
+    >
+      <Box>
+        <Image alt="Lamp" src={img1} width="300" height="200" />
+      </Box>
+      <Box sx={{ width: "100%" }}>
+        <Typography sx={{ color: "red", mt: 2, mb: 2 }}>
+          Dear Students,
+        </Typography>
+        <Typography sx={{ color: "blue" }}>
+          Greetings from College of Engineering & Technology, SRMIST !!
+        </Typography>
+      </Box>
+      <Box sx={{ border: "solid 1px #1E90FF", width: "70%", mt: 4, mb: 2 }}>
+        <Typography sx={{ color: "blue", mt: 2, mb: 2, fontSize: "28px" }}>
+          Students who are on internship with High Radius need not come to
+          campus physically. They will continue with online mode.
+        </Typography>
+      </Box>
+      <Box sx={{ border: "solid 1px #1E90FF", width: "100%", mt: 4, mb: 2 }}>
+        <Typography sx={{ color: "red", mt: 2, mb: 2, fontSize: "18px" }}>
+          👉You are hereby directed not to spam the email accounts of faculty
+          members, HoDs, Deans, Directors and other senior administrators of SRM
+          IST by sending the same content through multiple email accounts and
+          from non-srmist email (official email IDs) accounts by an individual.
+          This is going to be an unproductive effort.
+          <br />
+          <br /> You are also informed that any email coming from student&apos;s
+          personal email accounts will not be entertained and will be summarily
+          rejected or blocked.
+        </Typography>
+      </Box>
+      <Typography sx={{ m: 4 }}>Trulli</Typography>
+      <Box sx={{ border: "solid 1px #1E90FF", width: "100%" }}>
+        <Typography sx={{ color: "blue", mt: 2, mb: 2, fontSize: "20px" }}>
+          For Registration:
+          <a style={{ color: "red", textDecoration: "underline" }} href="#">
+            Click Here
+          </a>
+        </Typography>
+      </Box>
+      <Box sx={{ border: "solid 1px #1E90FF", width: "100%", mt: 0.25 }}>
+        <Typography sx={{ color: "blue", mt: 2, mb: 2, fontSize: "20px" }}>
+          Last date for submission:
+          <a style={{ color: "red" }}>7th March 2022.</a>
+        </Typography>
+      </Box>
+      <Box sx={{ border: "solid 1px #1E90FF", width: "100%", mt: 0.25, mb: 8 }}>
+        <Typography sx={{ color: "blue", mt: 2, mb: 2, fontSize: "20px" }}>
+          For Registration:
+          <a style={{ color: "red" }}>24th-26th March 2022.</a>
+        </Typography>
+      </Box>
+      <Box>
+        <Image alt="Lamp" src={img2} width="300" height="200" />
+      </Box>
+    </Box>
+  );
 };
 
 export default circular;
